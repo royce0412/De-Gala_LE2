520b6bc0c036d9d38ebd81bbd76d282556d75616
royce0412 <royce0412@gmail.com>
2024-05-13 11:50:30 +0800

    asdasd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 # Dictionary to store game library with their quantities and rental costs
 game_library = {
     "Donkey Kong": {"quantity": 3, "cost": 2},
@@ -13,13 +14,41 @@ user_accounts = {}
 admin_username = "admin"
 admin_password = "adminpass"
 
+# Function to use clear screen
+def Cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
 # Function to display available games with their numbers and rental costs
 def display_available_games():
-    pass
+    for index, game in enumerate(game_library, start = 1):
+        game_title = game_library[game]
+        game_quantity = game_library[game]["quantity"]
+        game_cost = game_library[game]["cost"]
+        print(f"{index}: {game_title}: Quantity: {game_quantity}, Cost: {game_cost}")        
 
 # Function to register a new user
 def register_user():
-    pass
+    print("Leave it blank in order to return")
+    print("Registering user.....")
+    
+    create_username = input("Username: ")
+    if create_username != "":    
+        create_password = input("Password: ")
+        if create_password != "":
+            account_details = {"Password": create_password, "Balance": 0, "Inventory": []}
+            user_accounts[create_username] = account_details
+            input("Account registered sucessfully!")
+            Cls()
+            main()
+        else:
+            input("Returning to main menu...")
+            Cls()
+            main()
+
+    else:
+        input("Returning to main menu...")
+        Cls()
+        main()
 
 # Function to rent a game
 def rent_game(username):
@@ -59,7 +88,22 @@ def display_game_inventory():
 
 # Function to handle user's logged-in menu
 def logged_in_menu(username):
-    pass
+    print("Logging in...")
+    
+    username = input("Username: ")
+    if username != "":
+        login_password = input("Password: ")
+        if login_password != "":
+        
+        else:
+            input("Returning to main menu...")
+            Cls()
+            main()
+    else:
+        input("Returning to main menu...")
+        Cls()
+        main()
+    
 
 # Function to check user credentials
 def check_credentials(username, password):
@@ -67,7 +111,30 @@ def check_credentials(username, password):
     
 # Main function to run the program
 def main():
-    pass
+    print("Welcome to the Game Rental Shop!")
+    print("1. User Login")
+    print("2. Admin Login")
+    print("3. Register User")
+    print("4. Exit")
+    main_choice = input("Choice: ")
+    Cls()
+    try:
+        if main_choice == "1":
+            logged_in_menu()
+        elif main_choice == "2":
+            admin_login()
+        elif main_choice == "3":
+            register_user()
+        elif main_choice == "4":
+            exit()
+        else:
+            raise Exception("Invalid choice. Please enter a number from 1 to 4.")
+    
+    except Exception as e:
+        input("Error: ", e)
 
+    
 if __name__ == "__main__":
     main()
+
+print(user_accounts)
